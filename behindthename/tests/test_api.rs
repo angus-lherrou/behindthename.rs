5f8a6f9bbfaa88f984a3dc0e893de30862fc28af
Angus L'Herrou <piraka@brandeis.edu>
2021-11-02 23:05:39 -0400

    feeding clippy

--- behindthename/tests/test_api.rs
+++ behindthename/tests/test_api.rs
@@ -9,18 +9,18 @@ use RateLimited::*;
 
 #[test]
 fn test_env_session_key() {
-    let key_string = env::var("BTN_API_KEY").unwrap_or("none".to_string());
+    let key_string = env::var("BTN_API_KEY").unwrap_or_else(|_| "none".to_string());
     let key = key_string.as_str();
     let sesh = session::Session::new_default(key);
     assert_eq!(
-        env::var("BTN_API_KEY").unwrap_or("none".to_string()),
+        env::var("BTN_API_KEY").unwrap_or_else(|_| "none".to_string()),
         sesh.key
     )
 }
 
 #[test]
 fn test_rate_limit() {
-    let key_string = env::var("BTN_API_KEY").unwrap_or("none".to_string());
+    let key_string = env::var("BTN_API_KEY").unwrap_or_else(|_| "none".to_string());
     let key = key_string.as_str();
     let sesh = session::Session::new_default(key);
     let req_1 = lookup::lookup("Jordan");
@@ -30,19 +30,18 @@ fn test_rate_limit() {
 
     match sesh.request(req_1) {
         Allowed(r) => println!("first request: {:?}", r.text().unwrap()),
-        Limited(i, n) => assert!(false, "first request failed: limiter {}, {:?}", i, n),
-        Error(e) => assert!(false, "first request failed: {:?}", e),
+        Limited(i, n) => panic!("first request failed: limiter {}, {:?}", i, n),
+        Error(e) => panic!("first request failed: {:?}", e),
     };
     match sesh.request(req_2) {
         Allowed(r) => println!("second request: {:?}", r.text().unwrap()),
-        Limited(i, n) => assert!(false, "second request failed: limiter {}, {:?}", i, n),
-        Error(e) => assert!(false, "second request failed: {:?}", e),
+        Limited(i, n) => panic!("second request failed: limiter {}, {:?}", i, n),
+        Error(e) => panic!("second request failed: {:?}", e),
     };
     // whether third or fourth request is the one that fails is up to chance
     match sesh.request(req_3) {
         Allowed(r) => match sesh.request(req_4) {
-            Allowed(s) => assert!(
-                false,
+            Allowed(s) => panic!(
                 "third and fourth requests succeeded: {:?}, {:?}",
                 r.text().unwrap(),
                 s.text().unwrap()
@@ -50,16 +49,16 @@ fn test_rate_limit() {
             Limited(i, n) => {
                 println! {"third request: {:?}; fourth request: limiter {}, {:?}", r.text().unwrap(), i, n}
             }
-            Error(e) => assert!(false, "fourth request failed: {:?}", e),
+            Error(e) => panic!("fourth request failed: {:?}", e),
         },
         Limited(i, n) => println! {"third request: limiter {}, {:?}", i, n},
-        Error(e) => assert!(false, "third request failed: {:?}", e),
+        Error(e) => panic!("third request failed: {:?}", e),
     };
 }
 
 #[test]
 fn test_json_lookup() {
-    let key_string = env::var("BTN_API_KEY").unwrap_or("none".to_string());
+    let key_string = env::var("BTN_API_KEY").unwrap_or_else(|_| "none".to_string());
     let key = key_string.as_str();
     let sesh = session::Session::new_default(key);
     let req = lookup::lookup("Jordan");
@@ -67,11 +66,11 @@ fn test_json_lookup() {
     match sesh.request_json(req) {
         Allowed(Okay(JsonResponseBody::NameDetails(e))) => println!("{:?}", e),
         Allowed(Okay(JsonResponseBody::NameList(e))) => {
-            assert!(false, "request parsed as name list: {:?}", e)
+            panic!("request parsed as name list: {:?}", e)
         }
-        Allowed(NotAvailable(_)) => assert!(false, "request service unavailable"),
-        Limited(i, n) => assert!(false, "request failed: limiter {}, {:?}", i, n),
-        Error(e) => assert!(false, "request failed: {:?}", e),
+        Allowed(NotAvailable(_)) => panic!("request service unavailable"),
+        Limited(i, n) => panic!("request failed: limiter {}, {:?}", i, n),
+        Error(e) => panic!("request failed: {:?}", e),
     };
 
     sleep(Duration::from_secs(2));
@@ -79,7 +78,7 @@ fn test_json_lookup() {
 
 #[test]
 fn test_json_random() {
-    let key_string = env::var("BTN_API_KEY").unwrap_or("none".to_string());
+    let key_string = env::var("BTN_API_KEY").unwrap_or_else(|_| "none".to_string());
     let key = key_string.as_str();
     let sesh = session::Session::new_default(key);
     let req = random::random_with_params(Male, "ita", 2, true);
@@ -87,11 +86,11 @@ fn test_json_random() {
     match sesh.request_json(req) {
         Allowed(Okay(JsonResponseBody::NameList(e))) => println!("{:?}", e),
         Allowed(Okay(JsonResponseBody::NameDetails(e))) => {
-            assert!(false, "request parsed as name details: {:?}", e)
+            panic!("request parsed as name details: {:?}", e)
         }
-        Allowed(NotAvailable(_)) => assert!(false, "request service unavailable"),
-        Limited(i, n) => assert!(false, "request failed: limiter {}, {:?}", i, n),
-        Error(e) => assert!(false, "request failed: {:?}", e),
+        Allowed(NotAvailable(_)) => panic!("request service unavailable"),
+        Limited(i, n) => panic!("request failed: limiter {}, {:?}", i, n),
+        Error(e) => panic!("request failed: {:?}", e),
     };
 
     sleep(Duration::from_secs(2));
@@ -99,7 +98,7 @@ fn test_json_random() {
 
 #[test]
 fn test_json_service_unavailable() {
-    let key_string = env::var("BTN_API_KEY").unwrap_or("none".to_string());
+    let key_string = env::var("BTN_API_KEY").unwrap_or_else(|_| "none".to_string());
     let key = key_string.as_str();
     let sesh = session::Session::new_default(key);
     let req_1 = lookup::lookup("Jordan");
@@ -109,13 +108,13 @@ fn test_json_service_unavailable() {
 
     match sesh.request(req_1) {
         Allowed(r) => println!("first request: {:?}", r.text().unwrap()),
-        Limited(i, n) => assert!(false, "first request failed: limiter {}, {:?}", i, n),
-        Error(e) => assert!(false, "first request failed: {:?}", e),
+        Limited(i, n) => panic!("first request failed: limiter {}, {:?}", i, n),
+        Error(e) => panic!("first request failed: {:?}", e),
     };
     match sesh.request(req_2) {
         Allowed(r) => println!("second request: {:?}", r.text().unwrap()),
-        Limited(i, n) => assert!(false, "second request failed: limiter {}, {:?}", i, n),
-        Error(e) => assert!(false, "second request failed: {:?}", e),
+        Limited(i, n) => panic!("second request failed: limiter {}, {:?}", i, n),
+        Error(e) => panic!("second request failed: {:?}", e),
     };
     // whether third or fourth request is the one that fails is up to chance
     match sesh.request_json(req_3) {
@@ -128,17 +127,13 @@ fn test_json_service_unavailable() {
                 (e.error_code, e.error),
                 (2usize, "service not available".to_string())
             ),
-            Allowed(Okay(s)) => assert!(
-                false,
-                "third and fourth requests succeeded: {:?}, {:?}",
-                r, s
-            ),
+            Allowed(Okay(s)) => panic!("third and fourth requests succeeded: {:?}, {:?}", r, s),
             Limited(i, n) => {
                 println! {"third request: {:?}; fourth request: limiter {}, {:?}", r, i, n}
             }
-            Error(e) => assert!(false, "fourth request failed: {:?}", e),
+            Error(e) => panic!("fourth request failed: {:?}", e),
         },
         Limited(i, n) => println! {"third request: limiter {}, {:?}", i, n},
-        Error(e) => assert!(false, "third request failed: {:?}", e),
+        Error(e) => panic!("third request failed: {:?}", e),
     };
 }
